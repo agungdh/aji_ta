cbbce37956e94e2d3f18390a242669fd8ddafeed
AgungDH <agungdh@live.com>
2019-06-26 13:40:19 +0700

    .

--- application/blades/template/menu.blade.php
+++ application/blades/template/menu.blade.php
@@ -17,31 +17,13 @@
 </li>
 
 <li>
-  <a href="{{ base_url() }}produkasuransiadmin">
-    <i class="fa fa-address-book"></i> <span>Produk Asuransi</span>
-  </a>
-</li>
-
-<li>
   <a href="{{ base_url() }}berita">
     <i class="fa fa-address-book"></i> <span>Berita/Informasi Terbaru</span>
   </a>
 </li>
 
 <li>
-  <a href="{{ base_url() }}beritaadmin">
-    <i class="fa fa-address-book"></i> <span>Berita/Informasi Terbaru</span>
-  </a>
-</li>
-
-<li>
   <a href="{{ base_url() }}agen">
-    <i class="fa fa-address-book"></i> <span>Data Agen</span>
-  </a>
-</li>
-
-<li>
-  <a href="{{ base_url() }}agenadmin">
     <i class="fa fa-address-book"></i> <span>Data Agen</span>
   </a>
 </li>
@@ -57,3 +39,36 @@
     <i class="fa fa-address-book"></i> <span>Kontak Alamat</span>
   </a>
 </li>
+
+@if(ci()->session->login && getUserData()->level == 'a')
+<li class="treeview">
+  <a href="#">
+    <i class="fa fa-users"></i>
+    <span>Admin</span>
+    <span class="pull-right-container">
+      <i class="fa fa-angle-left pull-right"></i>
+    </span>
+  </a>
+  <ul class="treeview-menu">
+
+    <li>
+      <a href="{{ base_url() }}produkasuransiadmin">
+        <i class="fa fa-address-book"></i> <span>Produk Asuransi</span>
+      </a>
+    </li>
+
+    <li>
+      <a href="{{ base_url() }}beritaadmin">
+        <i class="fa fa-address-book"></i> <span>Berita/Informasi Terbaru</span>
+      </a>
+    </li>
+
+    <li>
+      <a href="{{ base_url() }}agenadmin">
+        <i class="fa fa-address-book"></i> <span>Data Agen</span>
+      </a>
+    </li>
+
+  </ul>
+</li>
+@endif
